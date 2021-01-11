d059a961a8bbc34c80dae7e85a60ac00deaebe54
Dzarek <71340984+Dzarek@users.noreply.github.com>
2021-01-11 02:18:36 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,67 +1,90 @@
-const cardsColor = ['red', 'red', 'blue', 'blue', 'green', 'green', 'pink', 'pink', 'yellow', 'yellow', 'gray', 'gray', 'cadetblue', 'cadetblue', 'brown', 'brown', 'orange', 'orange'];
+const cardsColor = [
+  "red",
+  "red",
+  "blue",
+  "blue",
+  "green",
+  "green",
+  "pink",
+  "pink",
+  "yellow",
+  "yellow",
+  "gray",
+  "gray",
+  "cadetblue",
+  "cadetblue",
+  "brown",
+  "brown",
+  "orange",
+  "orange",
+];
 
-let cards = document.querySelectorAll('div');
+let cards = document.querySelectorAll("div");
 cards = [...cards];
-
 
 const startTime = new Date().getTime();
 
-let activeCard = '';
+let activeCard = "";
 const activeCards = [];
 
 const gamePairs = cards.length / 2;
 let gameResult = 0;
 
+const modal = document.querySelector(".modal");
 
+const modalBtn = document.querySelector(".modal .button");
+
+const modalTxt = document.querySelector(".modal .text");
 
 const clickCard = function () {
-    activeCard = this;
+  activeCard = this;
 
-    if (activeCard == activeCards[0]) return;
+  if (activeCard == activeCards[0]) return;
 
-    activeCard.classList.remove('hidden')
-    if (activeCards.length === 0) {
-        activeCards[0] = activeCard;
-        return;
-
-
-    } else {
-        cards.forEach(card => {
-            card.removeEventListener('click', clickCard)
-            activeCards[1] = activeCard;
-            setTimeout(function () {
-                if (activeCards[0].className === activeCards[1].className) {
-                    activeCards.forEach(card => card.classList.add('off'))
-                    gameResult++;
-                    cards = cards.filter(card => !card.classList.contains('off'))
-                    if (gameResult == gamePairs) {
-                        const endTime = new Date().getTime();
-                        const gameTime = (endTime - startTime) / 1000
-                        alert(`Udało się! Twój wynik to: ${gameTime} sekund`)
-                        location.reload();
-                    }
-                } else {
-                    activeCards.forEach(card => card.classList.add('hidden'))
-                }
-                activeCard = '';
-                activeCards.length = 0;
-                cards.forEach(card => card.addEventListener('click', clickCard))
-            }, 500)
-        })
-    }
-}
+  activeCard.classList.remove("hidden");
+  if (activeCards.length === 0) {
+    activeCards[0] = activeCard;
+    return;
+  } else {
+    cards.forEach((card) => {
+      card.removeEventListener("click", clickCard);
+      activeCards[1] = activeCard;
+      setTimeout(function () {
+        if (activeCards[0].className === activeCards[1].className) {
+          activeCards.forEach((card) => card.classList.add("off"));
+          gameResult++;
+          cards = cards.filter((card) => !card.classList.contains("off"));
+          if (gameResult == gamePairs) {
+            const endTime = new Date().getTime();
+            const gameTime = ((endTime - startTime) / 1000).toFixed(0);
+            modal.classList.remove("hide");
+            modalTxt.textContent = `Congratulation! You won this game in ${gameTime} second.`;
+          }
+        } else {
+          activeCards.forEach((card) => card.classList.add("hidden"));
+        }
+        activeCard = "";
+        activeCards.length = 0;
+        cards.forEach((card) => card.addEventListener("click", clickCard));
+      }, 500);
+    });
+  }
+};
 const init = () => {
-    cards.forEach(card => {
-        const position = Math.floor(Math.random() * cardsColor.length);
-        card.classList.add(cardsColor[position]);
-        cardsColor.splice(position, 1);
-    })
-    setTimeout(() => {
-        cards.forEach(function (card) {
-            card.classList.add('hidden')
-            card.addEventListener('click', clickCard)
-        })
-    }, 2000)
-}
+  cards.forEach((card) => {
+    const position = Math.floor(Math.random() * cardsColor.length);
+    card.classList.add(cardsColor[position]);
+    cardsColor.splice(position, 1);
+  });
+  setTimeout(() => {
+    cards.forEach(function (card) {
+      card.classList.add("hidden");
+      card.addEventListener("click", clickCard);
+    });
+  }, 2000);
+  modal.classList.add("hide");
+};
 
-init()
+init();
+
+modalBtn.addEventListener("click", () => location.reload());
